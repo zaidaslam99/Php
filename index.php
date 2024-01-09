d72445e12fa90f072cf20b4472b420afc3eacdcf
Zaid Aslam <122831945+zaidaslam99@users.noreply.github.com>
2024-01-08 20:01:11 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // This will print to the screen.
+        echo("Hello World!!");
+        echo "This is something else we can do";
+        echo "<hr>";
+        # This will show up as HTML in the screen.
+        echo "<h1>Zaid Site<h1>"
+    ?>
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $CharacterName = "John";
+    $CharacterAge = 50;
+
+    echo "There once was a man named $CharacterName <br>";
+    echo "He was $CharacterAge years old <br>";
+    echo "He really liked the name $CharacterName <br>";
+    echo "But didn't like being $CharacterAge <br>"; 
+    ?>
+</body>
+</html>
