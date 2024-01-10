6f38afbf8d4beaf772a0cd9f7cf0e136e75d428c
Zaid Aslam <122831945+zaidaslam99@users.noreply.github.com>
2024-01-09 20:12:42 -0600

    math functions

---
+++ working-with-numbers.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // echo 40;
+        // echo "<br>";
+        // echo 40.50;
+        // echo "<br>";
+        // echo 5 + 9;
+        // echo "<br>";
+        // echo 5.7 * 10;
+        // echo "<br>";
+        // echo 10 % 3;
+        // $num = 10;
+        // $num++;
+        // echo $num;
+        // $num = 10;
+        // $num += 25;
+        // echo $num;
+        // echo abs(-100);
+        // echo "<br>";
+        // echo pow(2, 4);
+        // echo "<br>";
+        // echo sqrt(144);
+        // echo "<br>";
+        // echo max(2, 10);
+        // echo "<br>";
+        // echo min(2, 10);
+        // echo "<br>";
+        // echo round(3.7);
+        // echo "<br>";
+        // echo ceil(3.3);
+        // echo "<br>";
+        // echo floor(3.5);
+    ?>
+</body>
+</html>
