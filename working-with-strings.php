d0a2cef8fc5421a5a39c668d6ede9b4ec79f6cb3
Zaid Aslam <122831945+zaidaslam99@users.noreply.github.com>
2024-01-09 19:42:21 -0600

    string maniplulation

--- working-with-strings.php
+++ working-with-strings.php
@@ -6,7 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
+    <?php
+    $phrase = "Giraffe Academy";
+    // echo strtolower($phrase);
+    // echo strtoupper($phrase);
+    // echo strlen($phrase);
+    // echo "<br>";
+    // echo $phrase[0];
+    // $phrase[0] = "b";
+    // echo $phrase;
+    // echo "<br>";
+    // echo "Mike"[0];
+    // echo "<br>";
+    // echo $phrase;
+    // echo "<br>";
+    // echo str_replace("Zaid", "Lebron", $phrase);
+    echo substr($phrase, 8, 3);
+
+
     ?>
 </body>
 </html>
